640d271412ffb36a388a45cfefc4d4c591ec60a0
habueb87 <satriaaliefpratama@gmail.com>
2022-12-13 09:37:24 +0700

    first commit

---
+++ lib/constants/icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// these are the initial category names and the icons for them
+Map<String, IconData> icons = {
+  'Auto And Transport': Icons.commute,
+  'Sports': Icons.sports_baseball,
+  'Food And Drinks': Icons.fastfood,
+  'Entertainment': Icons.local_play,
+  'Education': Icons.school,
+  'Other': Icons.pending,
+};
